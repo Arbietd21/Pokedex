50999cd1caef5edf3eb7906ea2de00485c27833d
Arbietd21 <arbiealemany14@gmail.com>
2023-10-06 00:13:07 -0500

    updated scripts.js
    added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,43 @@
-alert ('Hello World');
+let pokemonList = [
 
-let favoriteFood = 'Steak';
+    {name: 'Bulbasaur',
+    heigh: 0.7,
+    type: 'grass'},
 
-document.write(favoriteFood);
+    {name: 'Ivysaur',
+    heigh: 1,
+    type: ['grass', 'poison']},
+
+    {name: 'Venasaur',
+    heigh: 2,
+    type: 'grass'},
+
+    {name: 'Charmander',
+    heigh: 0.6,
+    type: 'fire'},
+
+    {name: 'Charmeleon',
+    heigh: 1.1,
+    type: 'fire'},
+
+    {name: 'Charizard',
+    heigh: 1.7,
+    type: 'dragon'},
+
+    {name: 'Squirtle',
+    heigh: 0.5,
+    type: 'water'},
+
+    {name: 'Wartortle',
+    heigh: 1,
+    type: 'water'},
+
+    {name: 'Blastoise',
+    heigh: 1.6,
+    type: 'water'},
+
+];
+
+let pikachu = {name: 'pikachu', height: 0.6, type: 'thunder'};
+
+console.log(pikachu [1]);
