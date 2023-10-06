cf1f871f8ff05c56ac0e40894fe5e0cd3ce7d3ef
Arbietd21 <arbiealemany14@gmail.com>
2023-10-06 01:15:48 -0500

    updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,41 @@
 let pokemonList = [
 
     {name: 'Bulbasaur',
-    heigh: 0.7,
+    height: 0.7,
     type: 'grass'},
 
     {name: 'Ivysaur',
-    heigh: 1,
+    height: 1,
     type: ['grass', 'poison']},
 
     {name: 'Venasaur',
-    heigh: 2,
+    height: 2,
     type: 'grass'},
 
     {name: 'Charmander',
-    heigh: 0.6,
+    height: 0.6,
     type: 'fire'},
 
     {name: 'Charmeleon',
-    heigh: 1.1,
+    height: 1.1,
     type: 'fire'},
 
     {name: 'Charizard',
-    heigh: 1.7,
+    height: 1.7,
     type: 'dragon'},
 
     {name: 'Squirtle',
-    heigh: 0.5,
+    height: 0.5,
     type: 'water'},
 
     {name: 'Wartortle',
-    heigh: 1,
+    height: 1,
     type: 'water'},
 
     {name: 'Blastoise',
-    heigh: 1.6,
+    height: 1.6,
     type: 'water'},
 
 ];
 
-let pikachu = {name: 'pikachu', height: 0.6, type: 'thunder'};
-
-console.log(pikachu [1]);
+console.log(pokemonList[1].type[1]);
